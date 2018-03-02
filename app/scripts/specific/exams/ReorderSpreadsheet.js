df824b1e8301aa2025070d286461628e821c2bb3
Haritz Medina <haritzmedina@gmail.com>
2018-03-02 11:20:09 +0100

    Fixed bug in ordering elements, when after total there are more columns with header.

--- app/scripts/specific/exams/ReorderSpreadsheet.js
+++ app/scripts/specific/exams/ReorderSpreadsheet.js
@@ -1,4 +1,5 @@
 const $ = require('jquery')
+const _ = require('lodash')
 
 class ReorderSpreadsheet {
   init () {
@@ -6,7 +7,14 @@ class ReorderSpreadsheet {
       if (err) {
 
       } else {
-        let criterias = _.without(_.map(data.data[0].rowData[0].values, (values) => { return values.formattedValue }), undefined, 'total').slice(1,-1)
+        // Get total index
+        let criterias = _.without(_.map(data.data[0].rowData[0].values, (values) => { return values.formattedValue }), undefined)
+        let totalIndex = _.findIndex(criterias, (criteria) => { return criteria.toLowerCase() === 'total' })
+        if (totalIndex > 0) {
+          criterias = criterias.slice(1, totalIndex) // maintain elements until total
+        } else {
+          criterias = criterias.slice(1) // total not found, maintain all except first one
+        }
         let container = window.abwa.tagManager.tagsContainer.annotate
         for (let i = criterias.length - 1; i >= 0; i--) {
           let criteria = criterias[i]
