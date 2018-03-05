c3f830bd2ee29975e5ce57d47cca64f58eee5b16
Haritz Medina <haritzmedina@gmail.com>
2018-03-05 13:04:44 +0100

    Minor improvements in reordering tag manager for exam example.

--- app/scripts/specific/exams/ReorderSpreadsheet.js
+++ app/scripts/specific/exams/ReorderSpreadsheet.js
@@ -2,10 +2,16 @@ const $ = require('jquery')
 const _ = require('lodash')
 
 class ReorderSpreadsheet {
-  init () {
+  init (callback) {
     window.abwa.specific.primaryStudySheetManager.getGSheetData((err, data) => {
+      let container = window.abwa.tagManager.tagsContainer.annotate
       if (err) {
-
+        container.dataset.examHidden = 'false' // Remove this tag after reordered tags
+        if (err) {
+          if (_.isFunction(callback)) {
+            callback(err)
+          }
+        }
       } else {
         // Get total index
         let criterias = _.without(_.map(data.data[0].rowData[0].values, (values) => { return values.formattedValue }), undefined)
@@ -15,17 +21,18 @@ class ReorderSpreadsheet {
         } else {
           criterias = criterias.slice(1) // total not found, maintain all except first one
         }
-        let container = window.abwa.tagManager.tagsContainer.annotate
         for (let i = criterias.length - 1; i >= 0; i--) {
           let criteria = criterias[i]
           let tagGroup = _.find(container.querySelectorAll('.tagGroup'), (elem) => { return elem.children[0].title === criteria })
           let elem = $(tagGroup).detach()
           $(container).prepend(elem)
-          $(container).show()
+          container.dataset.examHidden = 'false' // Remove this tag after reordered tags
+        }
+        if (_.isFunction(callback)) {
+          callback()
         }
       }
-      console.log(data)
-    })
+    }, false)
   }
 }
 
