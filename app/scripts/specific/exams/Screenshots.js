1d5ecf37d76f1c94c44c7305f9242fd10d539da5
Haritz Medina <haritzmedina@gmail.com>
2018-11-30 16:17:08 +0100

    Screenshots for pdf working using jspdf

--- app/scripts/specific/exams/Screenshots.js
+++ app/scripts/specific/exams/Screenshots.js
@@ -1,7 +1,9 @@
 const html2canvas = require('html2canvas')
+window.html2canvas = require('html2canvas')
 const FileSaver = require('file-saver')
 const $ = require('jquery')
 const _ = require('lodash')
+const JsPDF = require('jspdf')
 
 class Screenshots {
   constructor () {
@@ -39,6 +41,43 @@ class Screenshots {
             resolve(canvas)
           })
         }
+      })
+    } else if (window.location.pathname === '/content/pdfjs/web/viewer.html') {
+      // Create promises array
+      let promisesData = [...Array(window.PDFViewerApplication.pagesCount).keys()].map((index) => { return {i: index} })
+
+      let takePDFPageScreenshot = (d, chaindata) => {
+        return new Promise((resolve, reject) => {
+          // Go to page
+          window.PDFViewerApplication.page = d.i + 1
+          // Redraw annotations
+          window.abwa.contentAnnotator.redrawAnnotations()
+          setTimeout(() => {
+            html2canvas(document.querySelector('.page[data-page-number="' + (d.i + 1) + '"]')).then((canvas) => {
+              chaindata.push(canvas)
+              resolve(chaindata)
+            })
+          }, 1000)
+        })
+      }
+
+      let promiseChain = promisesData.reduce(
+        (chain, d) => chain.then((chaindata) => {
+          return takePDFPageScreenshot(d, chaindata)
+        }), Promise.resolve([])
+      )
+      promiseChain.then((canvases) => {
+        let pdf = new JsPDF('p', 'pt', 'a4')
+        window.abwa.tagManager.showViewingTagsContainer()
+        html2canvas(document.querySelector('#tagsViewing')).then((rubric) => {
+          pdf.addImage(rubric.toDataURL(), 'png', 0, 0)
+          window.abwa.tagManager.showEvidencingTagsContainer()
+          for (let i = 0; i < canvases.length; i++) {
+            pdf.addPage()
+            pdf.addImage(canvases[i].toDataURL(), 'png', 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight())
+          }
+          pdf.save()
+        })
       })
     } else {
       promise = new Promise((resolve) => {
